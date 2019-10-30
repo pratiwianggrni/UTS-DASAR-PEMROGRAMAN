9a61f6aa470e59127d0d5298a03a758a86d7ced2
pratiwianggrni <prtwanggraeni@gmail.com>
2019-10-30 10:53:20 +0700

    UTS DASAR PEMROGRAMAN

---
+++ BAB VI/pengulanganwhile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int pencacahan = 1;
+    while (pencacahan <=10)
+    {
+        cout << "Teknik Informatika STMIK Sumedang \n";
+        pencacahan++;
+    }
+
+    return 0;
+}
